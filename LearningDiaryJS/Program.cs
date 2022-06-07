64a7dd74c293a4b410762fee8c2f581b766b5ddc
jaakkosy <jaakkosyrjamaki92@hotmail.com>
2022-06-07 13:29:00 +0300

    started project

---
+++ LearningDiaryJS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningDiaryJS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
